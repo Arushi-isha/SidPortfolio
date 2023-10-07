4433aab3def49915e849b477082c5c14fc160f55
Arushi~Isha <arushimallickisha@gmail.com>
2023-10-07 14:31:11 +0530

    commit

---
+++ portfoliojs.js
@@ -0,0 +1,17 @@
+const toggle = document.getElementById('toggle');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function () {
+    if (this.classList.contains('fa-sun')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    }
+
+    this.classList.toggle('fa-solid');
+    this.classList.toggle('fa-moon');
+    this.classList.toggle('fa-regular');
+    this.classList.toggle('fa-sun');
+});
